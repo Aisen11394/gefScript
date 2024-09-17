4ea787749e94cfd35306a8bee3e30410fd3b2a37
Aisen11394 <154973366+Aisen11394@users.noreply.github.com>
2024-09-17 22:07:50 +0900

    Update .lua

--- .lua
+++ .lua
@@ -7,6 +7,7 @@ local gui = loadstring(game:HttpGet("https://gitlab.com/0x45.xyz/droplib/-/raw/m
 gui:CreateCategory("Clean Up"):CreateButton("Click",function() gui:CleanUp() end)
 local lpg = gui:CreateCategory("Local Player")
 
-lpg:CreateSlider("Walk Speed", function(ws) game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = ws end,0,1000,nil,true,game.Players.LocalPlayer.Character.Humanoid.WalkSpeed)
-lpg:CreateSlider("Jump Power", function(jp) game.Players.LocalPlayer.Character.Humanoid.JumpPower = jp end,0,2000,nil,true,game.Players.LocalPlayer.Character.Humanoid.JumpPower)
+lpg:CreateSlider("Walk Speed", function(ws) game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = ws end,0,100,nil,true,game.Players.LocalPlayer.Character.Humanoid.WalkSpeed)
+lpg:CreateSlider("Jump Power", function(jp) game.Players.LocalPlayer.Character.Humanoid.JumpPower = jp end,0,200,nil,true,game.Players.LocalPlayer.Character.Humanoid.JumpPower)
 lpg:CreateButton("Force Field", function() Instance.new("ForceField",game.Players.LocalPlayer.Character) end)
+lpg:CreateSlider("Health", function(h) game.Players.LocalPlayer.Character.Humanoid.Health = h end,0,200,nil,true,game.Players.LocalPlayer.Character.Humanoid.Health)
